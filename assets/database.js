85f1e91bfa58826599a201f1d229cfdd1988437c
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-08-31 17:52:00 -0300

    atualizando atividades

--- assets/database.js
+++ assets/database.js
@@ -1,18 +1,32 @@
 const database = [
     {
         "title": "HTML 1",
-        "url": "01/minhaPagina.html"
+        "url": "modulo01/minhaPagina.html",
+        "module": 1
     },
     {
         "title": "Formulários",
-        "url": "02/formularios1.html"
+        "url": "modulo01/formularios1.html",
+        "module": 1
     },
     {
         "title": "Formulários 2",
-        "url": "02/formularios2.html"
+        "url": "modulo02/formularios2.html",
+        "module": 2
     },
     {
         "title": "Imóveis",
-        "url": "03/imoveis.html"
+        "url": "modulo02/imoveis.html",
+        "module": 2
+    },
+    {
+        "title": "Menu",
+        "url": "modulo02/menu.html",
+        "module": 2
+    },
+    {
+        "title": "Clínica",
+        "url": "modulo02/clinica.html",
+        "module": 2
     },
 ]
--- assets/scripts.js
+++ assets/scripts.js
@@ -4,11 +4,29 @@ database.forEach(exercise => {
     var title = document.createElement('p');
     title.innerText = exercise.title;
 
+    var exModule = document.createElement('span');
+    exModule.innerText = exercise.module;
+    switch (exercise.module) {
+        case 1:
+            exModule.style.backgroundColor = 'green';
+            break;
+        case 2:
+            exModule.style.backgroundColor = 'orange';
+            break;
+        case 3:
+            exModule.style.backgroundColor = 'red';
+            break;
+        case 4:
+            exModule.style.backgroundColor = 'blue';
+            break;
+    }
+
     var element = document.createElement('div');
     element.classList.add('item');
     element.addEventListener('click', function (e) {
         window.location = exercise.url;
     }, false);
+    element.appendChild(exModule);
     element.appendChild(title);
 
     container.appendChild(element);
