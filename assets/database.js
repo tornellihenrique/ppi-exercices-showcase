a670e8b467edd3b1ce1c0ced86a0974c758a0964
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-09-05 22:12:57 -0300

    atualizando atividades

--- assets/database.js
+++ assets/database.js
@@ -29,4 +29,24 @@ const database = [
         "url": "modulo02/clinica.html",
         "module": 2
     },
+    {
+        "title": "JS 1",
+        "url": "modulo03/js1.html",
+        "module": 3
+    },
+    {
+        "title": "JS 2",
+        "url": "modulo03/js2.html",
+        "module": 3
+    },
+    {
+        "title": "JS 3",
+        "url": "modulo03/js3.html",
+        "module": 3
+    },
+    {
+        "title": "JS 4",
+        "url": "modulo03/js4.html",
+        "module": 3
+    },
 ]
